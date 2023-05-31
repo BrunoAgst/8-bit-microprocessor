bedd81dfd67a1ac7149633fcb2af85076bdab45c
Bruno Augusto <bruno_agst@hotmail.com>
2023-05-30 23:37:26 -0300

    added an animation

--- headers.h
+++ headers.h
@@ -1,16 +1,18 @@
 // NOTE: instructions
+#define LDA 0x03
 #define LDI 0x04
 #define ADD 0x05
 #define SUB 0x06
-#define NULL 0x00
 
 // NOTE: functions
+void print_init();
 void fetch_cycle();
 void get_instruction();
 void search_instruction();
 int select_instruction(char opcode);
 void search_operation(char instruct);
+void lda_exec(); // TODO: implement lda statement
 void ldi_exec();
 void add_exec();
-void sub_exec(); // TODO: Implement the sub statement
+void sub_exec();
 void print_output();
--- main.c
+++ main.c
@@ -1,7 +1,29 @@
 #include "micro_processor.c"
+#include "headers.h"
 
 int main()
 {
+    print_init();
     fetch_cycle();
     return 0;
+}
+
+void print_init()
+{
+    printf("\n\n");
+    printf(" ####   ####  #    # #####  #    # ##### ###### #####   \n");
+    printf("#    # #    # ##  ## #    # #    #   #   #      #    #  \n");
+    printf("#      #    # # ## # #    # #    #   #   #####  #    #  \n");
+    printf("#      #    # #    # #####  #    #   #   #      #####   \n");
+    printf("#    # #    # #    # #      #    #   #   #      #   #   \n");
+    printf(" ####   ####  #    # #       ####    #   ###### #    #  \n");
+    printf("\n\n");
+    printf("          #####                                  \n");
+    printf("         #     #       #####   #  #####          \n");
+    printf("         #     #       #    #  #    #            \n");
+    printf("          #####  ##### #####   #    #            \n");
+    printf("         #     #       #    #  #    #            \n");
+    printf("         #     #       #    #  #    #            \n");
+    printf("          #####        #####   #    #            \n");
+    printf("\n\n");
 }
